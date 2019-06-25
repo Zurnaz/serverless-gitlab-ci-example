7d0e3f90785abb843a77eb0f70e805a46b033b7d
Bogdan Drema <zurnaz@gmail.com>
2019-06-25 10:45:03 +0100

    Swapped to middy middleware and added unit tests for feature one

--- src/feature-one/handler.js
+++ src/feature-one/handler.js
@@ -1,8 +1,49 @@
 module.exports.AWS = require('aws-sdk')
-const generatejson = require('../utils/utils').generateJsonResponse
+const middy = require('middy')
+const { validator, jsonBodyParser } = require('middy/middlewares')
 
-module.exports.get = async (event, context) => {
-  const response = 'Feature one response'
-  const generated = generatejson(200, response)
-  return generated
+// Middy example grabbed from https://middy.js.org/docs/middlewares.html
+const outSchema = {
+  required: ['body', 'statusCode'],
+  properties: {
+    body: {
+      type: 'string',
+    },
+    statusCode: {
+      type: 'number',
+    },
+  },
 }
+
+const get = middy(async (event, context) => {
+  const response = {
+    body: JSON.stringify({ result: 'Feature one response' }),
+    statusCode: 200,
+  }
+  return response
+})
+
+get.use(validator({ outputSchema: outSchema }))
+
+const post = middy(async (event, context) => {
+  const response = {
+    body: JSON.stringify({ result: event.body.text }),
+    statusCode: 200,
+  }
+  if (event.body.text === 0) {
+    response.statusCode = 'text'
+  }
+  return response
+})
+
+const inSchema = {
+  required: ['body'],
+  properties: {
+    body: {
+      type: 'string',
+    },
+  },
+}
+post.use(validator({ inputSchema: inSchema, outputSchema: outSchema }))
+post.use(jsonBodyParser())
+module.exports = { get, post }
--- src/feature-one/handler.unit.js
+++ src/feature-one/handler.unit.js
@@ -1,19 +1,58 @@
 const handler = require('./handler')
-const generateJsonResponse = require('../utils/utils').generateJsonResponse
 
 beforeAll(() => {})
 
 afterAll(() => {})
 
 describe('get function', () => {
-  it('returns the correct response', async () => {
-    const expectedBody = generateJsonResponse(200, 'Feature one response')
-    expect.assertions(1)
-    try {
-      const data = await handler.get()
-      expect(data).toEqual(expectedBody)
-    } catch (e) {
-      console.log(e)
+  it('returns the correct response', async (endTest) => {
+    const expectedBody = JSON.stringify({ result: 'Feature one response' })
+    handler.get({}, {}, (err, response) => {
+      expect(response.statusCode).not.toBe(null)
+      expect(response.body).not.toBe(null)
+      expect(response.body).toEqual(expectedBody)
+      expect(err).toBe(null)
+      endTest()
+    })
+  })
+})
+
+describe('post function', () => {
+  it('Validates input schema gets', async (endTest) => {
+    const event = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: 'This is text' }),
     }
+    handler.post(event, {}, (err, response) => {
+      expect(response.body).toEqual(JSON.stringify({ result: 'This is text' }))
+      expect(err).toBe(null)
+      endTest()
+    })
+  })
+
+  it('Causes an input schema related error', async (endTest) => {
+    handler.post({}, {}, (err, response) => {
+      expect(err).not.toBe(null)
+      // TODO fix this, no clue whats going wrong
+      expect(err.message).toEqual('Event object failed validation')
+      endTest()
+    })
+  })
+
+  it('Causes an output schema related error', async (endTest) => {
+    const event = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: 0 }),
+    }
+    handler.post(event, {}, (err, response) => {
+      expect(err).not.toBe(null)
+      // TODO fix this, no clue whats going wrong
+      expect(err.message).toEqual('Response object failed validation')
+      endTest()
+    })
   })
 })
