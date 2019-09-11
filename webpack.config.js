a57b94d8da496be15da58d302daac20ac1ff8d61
Bogdan Drema <zurnaz@gmail.com>
2019-09-12 00:54:45 +0100

    Version bump and dev/prod  mode for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,5 @@ const slsw = require('serverless-webpack')
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
 }
